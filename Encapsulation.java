2bfda82e8b92887b7c7072fc4e4a0ddbbbfcbdad
Akkinepally Vishnupriya <a.vishnupriya@jsw.in>
2025-02-14 21:40:55 +0530

    encap

--- Encapsulation.java
+++ Encapsulation.java
@@ -2,10 +2,26 @@
 //it means no one outside the class can access it.--declare as private
 class Stud
 {
-    private int age=20;
+    private int age;
+
+    //constructor--has same name as class name, no return type
+    //is called everytime an object to that class is created
+    //used for default variables/instance variables initialization
+    //and to connect to databases
+    //to perform any operation/assigning values--do it in methods
+    public Stud() //default constructor
+    { 
+        age=20;
+        System.out.println("Am in constructor");
+    }
+    public Stud(int age)
+    {
+        this.age=age;
+    }
     int pri()
     {
        // System.out.println(age);
+       System.out.println("Am in method pri");
        return age;
     }
    /* void setage(int x) //always method name should be readable// like gettters to get value, set for setting value--jordans method
@@ -24,10 +40,19 @@ class Encapsulation
     public static void main(String a[])
     {
            Stud s1=new Stud();
-           s1.setage(24); //to modify that vraiable also we need mthod in it
-           System.out.println(s1.pri()));
+           Stud s2=new Stud();
+          // s1.setage(24); //to modify that vraiable also we need mthod in it
+           System.out.println(s1.pri());
           // System.out.println(s1.age);//throws error as we cannot access age as its private to that class
            //but we can access it through the method
+
+           //anonymous object
+           //whenever we create a new object without any reference--its an anonymous object
+
+           new Stud().pri();  //object created--constructor will be called---but it has no reference--it means we cant use it again
+
+           //but we can use it to call a method once
+           //cannot bereused, as object already created
 
     }
 }
